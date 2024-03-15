8cbe44e83ba2261f594f0f4c2056dda6d43a535d
Victoria Kharlanchuk <kaplabetty@gmail.com>
2024-03-15 14:02:55 +0100

    [6CORE-027] add interface & ProductResponseDTO

--- 3-platform/warehouse/spec/src/main/java/com/_6core/platform/warehouse/rest/v1/dto/ProductResponseDto.java
+++ 3-platform/warehouse/spec/src/main/java/com/_6core/platform/warehouse/rest/v1/dto/ProductResponseDto.java
@@ -1,2 +1,12 @@
-package com._6core.platform.warehouse.rest.v1.dto;public class ProductResponseDto {
+package com._6core.platform.warehouse.rest.v1.dto;
+
+import lombok.Data;
+import java.math.BigDecimal;
+
+@Data
+public class ProductResponseDto {
+   private Long Id;
+    private String name;
+    private BigDecimal price;
+    private String description;
 }
