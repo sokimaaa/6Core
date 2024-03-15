7b8896f6e5914c88d0490e36d448eb41a43d4e7f
Kateryna Mykhailovska <mkhlvskate@gmail.com>
2024-03-15 22:24:17 +0200

    [6CORE-022] fixed minor requested changes

--- 3-platform/order/spec/src/main/java/com/_6core/platform/orderspec/rest/v1/OrderApi.java
+++ 3-platform/order/spec/src/main/java/com/_6core/platform/orderspec/rest/v1/OrderApi.java
@@ -11,13 +11,10 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 import reactor.core.publisher.Flux;
 
 @Tag(name = "Order Specification", description = "REST reactive specification for interaction with Order service")
 public interface OrderApi {
-    // Dummy property right now. Should be bringing to .property?
-    final String version = "6core.com.orders.v1";
 
     /**
      * Searching orders history by single user GET /orders
@@ -25,14 +22,14 @@ public interface OrderApi {
      * @param acceptHeader - (while request should use application version)
      * @return OK (status code 200)
      */
-    @GetMapping(value = "/orders", produces = "application/" + version + "+json")
+    @GetMapping(value = "/orders", produces = "application/json")
     @Operation(summary = "Get orders history", description = "Get all orders with all statuses by a single user")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "OK", content = {
                     @Content(mediaType = "application/json", schema = @Schema(implementation = OrderResponse.class)) }),
-            @ApiResponse(responseCode = "401", description = "Authorization information is missing or invalid", content = @Content),
-            @ApiResponse(responseCode = "5ХХ", description = "Unexpected error", content = @Content) })
-    public default Flux<ResponseEntity<OrderResponse>> getOrdersHistory(@RequestHeader("Accept") String acceptHeader) {
+            @ApiResponse(responseCode = "401", description = "Authorization information is missing or invalid"),
+            @ApiResponse(responseCode = "500", description = "Unexpected error") })
+    default Flux<ResponseEntity<OrderResponse>> getOrdersHistory() {
         return Flux.empty();
     }
 
@@ -43,18 +40,17 @@ public interface OrderApi {
      * @param acceptHeader - (while request should use application version)
      * @return OK (status code 200)
      */
-    @GetMapping(value = "/orders/{orderId}", produces = "application/" + version + "+json")
+    @GetMapping(value = "/orders/{orderId}/items", produces = "application/json")
     @Operation(summary = "Get order details", description = "Get details of a specific order by order ID")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "OK", content = {
                     @Content(mediaType = "application/json", schema = @Schema(implementation = OrderResponse.class)) }),
-            @ApiResponse(responseCode = "400", description = "Bad request. Order ID must be an long and larger than 0", content = @Content),
-            @ApiResponse(responseCode = "401", description = "Authorization information is missing or invalid", content = @Content),
-            @ApiResponse(responseCode = "404", description = "Order not found", content = @Content),
-            @ApiResponse(responseCode = "5ХХ", description = "Unexpected error", content = @Content) })
-    public default Flux<ResponseEntity<OrderResponse>> getOrder(
-            @Parameter(description = "ID of the order", required = true) @PathVariable Long orderId,
-            @RequestHeader("Accept") String acceptHeader) {
+            @ApiResponse(responseCode = "400", description = "Bad request. Order ID must be an long and larger than 0"),
+            @ApiResponse(responseCode = "401", description = "Authorization information is missing or invalid"),
+            @ApiResponse(responseCode = "404", description = "Order not found"),
+            @ApiResponse(responseCode = "500", description = "Unexpected error") })
+    default Flux<ResponseEntity<OrderResponse>> getOrder(
+            @Parameter(description = "ID of the order", required = true) @PathVariable Long orderId) {
         return Flux.empty();
     }
 }
