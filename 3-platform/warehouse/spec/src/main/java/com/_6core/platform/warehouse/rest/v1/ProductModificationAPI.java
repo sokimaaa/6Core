13b66565d66c76965281277d561349bc644e2957
Victoria Kharlanchuk <kaplabetty@gmail.com>
2024-03-17 21:07:18 +0100

    [6CORE-027] resolved remarks and return to use one interface

--- 3-platform/warehouse/spec/src/main/java/com/_6core/platform/warehouse/rest/v1/ProductModificationAPI.java
+++ 3-platform/warehouse/spec/src/main/java/com/_6core/platform/warehouse/rest/v1/ProductModificationAPI.java
@@ -1,31 +1,84 @@
 package com._6core.platform.warehouse.rest.v1;
 
+import com._6core.platform.warehouse.rest.v1.dto.product.RequestProductDescription;
+import com._6core.platform.warehouse.rest.v1.dto.product.RequestProductName;
+import com._6core.platform.warehouse.rest.v1.dto.product.RequestProductPrice;
+import com._6core.platform.warehouse.rest.v1.dto.product.ResponseProductDescription;
+import com._6core.platform.warehouse.rest.v1.dto.product.ResponseProductName;
+import com._6core.platform.warehouse.rest.v1.dto.product.ResponseProductPrice;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import reactor.core.publisher.Mono;
+import java.math.BigDecimal;
 
 /**
  * Interface for modifying product information in the Warehouse Service.
  */
 
 @Tag(name = "Product Modification", description = "REST specification for modifying product information")
-public interface ProductModificationAPI<T, K> {
+public interface ProductModificationAPI {
     /**
-     * Updates product's name, description and price for a given product identified by its ID.
+     * Update product's description for a given product identified by its ID.
      */
-    @Operation(summary = "Update product", description = "Modify details of a product")
+    @Operation(summary = "Update product description", description = "Modify the description of a product")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "200", description = "OK", content = {
+                    @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ResponseProductDescription.class))}),
             @ApiResponse(responseCode = "404", description = "Product not found"),
             @ApiResponse(responseCode = "500", description = "Internal server error")
     })
-    @PatchMapping("/products/{productId}")
-    Mono<ResponseEntity<T>> updateProduct(@PathVariable("productId") Long productId,@RequestBody K request);
+    @PatchMapping("/products/{productId}/description")
+    default Mono<ResponseEntity<ResponseProductDescription>> updateProduct(Long productId,
+                                                                           RequestProductDescription request) {
+
+        ResponseProductDescription response
+                = new ResponseProductDescription(-1L, "updated");
+        return Mono.just(ResponseEntity.ok(response));
+    }
+
+    /**
+     * Update product's name for a given product identified by its ID.
+     */
+    @Operation(summary = "Update product description", description = "Modify the description of a product")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK", content = {
+                    @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ResponseProductName.class))}),
+            @ApiResponse(responseCode = "404", description = "Product not found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    @PatchMapping("/products/{productId}/name")
+    default Mono<ResponseEntity<ResponseProductName>> updateProduct(Long productId,
+                                                                    RequestProductName request) {
+        ResponseProductName response
+                = new ResponseProductName(-1L, "updated");
+        return Mono.just(ResponseEntity.ok(response));
+    }
+
+    /**
+     * Update product's price for a given product identified by its ID
+     */
+
+    @Operation(summary = "Update product description", description = "Modify the description of a product")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK", content = {
+                    @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ResponseProductPrice.class))}),
+            @ApiResponse(responseCode = "404", description = "Product not found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    @PatchMapping("/products/{productId}/price")
+    default Mono<ResponseEntity<ResponseProductPrice>> updateProduct(Long productId,
+                                                                     RequestProductPrice request) {
+        ResponseProductPrice response
+                = new ResponseProductPrice(-1L, new BigDecimal("123.45"));
+        return Mono.just(ResponseEntity.ok(response));
+    }
 }
