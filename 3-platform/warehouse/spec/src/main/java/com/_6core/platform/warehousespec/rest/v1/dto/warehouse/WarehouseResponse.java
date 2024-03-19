9d0ee26c25b7d4690ef1a12676517ef85d5b107d
Yura Khilko <yurakhilko@gmail.com>
2024-03-19 20:43:40 +0200

    [6CORE-023] renamed WarehouseResponse field.

--- 3-platform/warehouse/spec/src/main/java/com/_6core/platform/warehousespec/rest/v1/dto/warehouse/WarehouseResponse.java
+++ 3-platform/warehouse/spec/src/main/java/com/_6core/platform/warehousespec/rest/v1/dto/warehouse/WarehouseResponse.java
@@ -3,7 +3,7 @@ package com._6core.platform.warehousespec.rest.v1.dto.warehouse;
 import java.math.BigDecimal;
 
 public record WarehouseResponse(
-        Long productId,
+        Long warehouseId,
         String name,
         String description,
         Long addressId,
