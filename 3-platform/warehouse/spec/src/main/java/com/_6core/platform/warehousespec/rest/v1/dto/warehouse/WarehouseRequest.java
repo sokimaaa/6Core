746508ea0387540d66b0cc47843b9ee1a2315237
Yura Khilko <yurakhilko@gmail.com>
2024-03-20 20:18:45 +0200

    [6CORE-055] Fixed REST Specification for Warehouse Service: Create Warehouse.

--- 3-platform/warehouse/spec/src/main/java/com/_6core/platform/warehousespec/rest/v1/dto/warehouse/WarehouseRequest.java
+++ 3-platform/warehouse/spec/src/main/java/com/_6core/platform/warehousespec/rest/v1/dto/warehouse/WarehouseRequest.java
@@ -1,10 +1,6 @@
 package com._6core.platform.warehousespec.rest.v1.dto.warehouse;
 
-import java.math.BigDecimal;
-
 public record WarehouseRequest(
-        String name,
-        String description,
-        Long addressId) {
+        String address) {
 
 }
--- 3-platform/warehouse/spec/src/main/java/com/_6core/platform/warehousespec/rest/v1/dto/warehouse/WarehouseResponse.java
+++ 3-platform/warehouse/spec/src/main/java/com/_6core/platform/warehousespec/rest/v1/dto/warehouse/WarehouseResponse.java
@@ -1,11 +1,6 @@
 package com._6core.platform.warehousespec.rest.v1.dto.warehouse;
 
-import java.math.BigDecimal;
-
 public record WarehouseResponse(
-        Long warehouseId,
-        String name,
-        String description,
-        Long addressId,
-        String status) {
+        String warehouseId,
+        String address) {
 }
