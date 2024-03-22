bebb8f7da80624eb803133878c70ef53c7b7df62
Victoria Kharlanchuk <kaplabetty@gmail.com>
2024-03-22 22:11:29 +0100

    [6CORE-024] checkstyle

--- 3-platform/warehouse/spec/src/main/java/com/_6core/platform/warehousespec/rest/v1/InventoryManagementApi.java
+++ 3-platform/warehouse/spec/src/main/java/com/_6core/platform/warehousespec/rest/v1/InventoryManagementApi.java
@@ -13,7 +13,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import reactor.core.publisher.Mono;
 
 /**
