1e198f5173ea5eb1da3526702703eb4629d33590
Kateryna Mykhailovska <mkhlvskate@gmail.com>
2024-03-24 05:10:27 +0200

    [6CORE-022] add OrderDetailsResponse dto

--- 3-platform/order/spec/src/main/java/com/_6core/platform/orderspec/rest/v1/dto/OrderItemResponse.java
+++ 3-platform/order/spec/src/main/java/com/_6core/platform/orderspec/rest/v1/dto/OrderItemResponse.java
@@ -3,9 +3,4 @@ package com._6core.platform.orderspec.rest.v1.dto;
 import java.math.BigInteger;
 
 public record OrderItemResponse(
-        String itemId,
-        String productId,
-        Integer quantity,
-        BigInteger price) {
-
-}
+    String itemId, String productId, Integer quantity, BigInteger price) {}
--- 3-platform/order/spec/src/main/java/com/_6core/platform/orderspec/rest/v1/dto/OrderResponse.java
+++ 3-platform/order/spec/src/main/java/com/_6core/platform/orderspec/rest/v1/dto/OrderResponse.java
@@ -2,9 +2,4 @@ package com._6core.platform.orderspec.rest.v1.dto;
 
 import java.math.BigInteger;
 
-public record OrderResponse(
-        String orderId, 
-        String status, 
-        BigInteger total, 
-        String userId){
-}
+public record OrderResponse(String orderId, String status, BigInteger total, String userId) {}
