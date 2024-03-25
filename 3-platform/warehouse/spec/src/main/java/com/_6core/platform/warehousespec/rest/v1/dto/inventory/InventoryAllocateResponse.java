1ca06dd9b86b113498aa8603dc00f46882ec4c35
Victoria Kharlanchuk <kaplabetty@gmail.com>
2024-03-25 14:08:45 +0100

    [6core-024] fixed checkstyle

--- 3-platform/warehouse/spec/src/main/java/com/_6core/platform/warehousespec/rest/v1/dto/inventory/InventoryAllocateResponse.java
+++ 3-platform/warehouse/spec/src/main/java/com/_6core/platform/warehousespec/rest/v1/dto/inventory/InventoryAllocateResponse.java
@@ -1,7 +1,4 @@
 package com._6core.platform.warehousespec.rest.v1.dto.inventory;
 
-public record InventoryAllocateResponse( String locateId,
-                                         String warehouseId,
-                                         String inventoryId,
-                                         Integer quantity) {
-}
+public record InventoryAllocateResponse(
+    String locateId, String warehouseId, String inventoryId, Integer quantity) {}
