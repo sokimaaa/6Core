1aa36ffddafe638acdd0528a46a0d104867d026e
Kateryna Mykhailovska <mkhlvskate@gmail.com>
2024-03-25 13:28:57 +0200

    [6CORE-026] fixed delimeters

--- 3-platform/warehouse/spec/src/main/java/com/_6core/platform/warehousespec/rest/v1/ProductGetApi.java
+++ 3-platform/warehouse/spec/src/main/java/com/_6core/platform/warehousespec/rest/v1/ProductGetApi.java
@@ -18,54 +18,82 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import reactor.core.publisher.Flux;
 
-@Tag(name = "Product Receive Specification", description = "REST reactive specification for getting products from warehouse service")
+@Tag(
+    name = "Product Receive Specification",
+    description = "REST reactive specification for getting products from warehouse service")
 public interface ProductGetApi {
-    /*
-     * - Design filtering feature
-     */
 
-    // referance:
-    // https://rozetka.com.ua/lepka4632166/c4632166/detskiy-vozrast=14-let,18-let;price=129-189;producer=danko-toys/
-    /**
-     * Exploring the products by filter criteria. Provide all products with given
-     * filters. If any tags provided then return all products.
-     * 
-     * @param filterCreteria (non required params)
-     * @return OK (status code 200)
-     */
-    @GetMapping(value = "/products", produces = "application/json")
-    @Operation(summary = "Get products by filter creteria", description = "Get all products by specified filters")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "OK", content = {
-                    @Content(mediaType = "application/json", schema = @Schema(implementation = ProductCartResponse.class)) }),
-            @ApiResponse(responseCode = "400", description = "Bad request. Requested parameters doesn't exist"),
-            @ApiResponse(responseCode = "401", description = "Authorization information is missing or invalid"),
-            @ApiResponse(responseCode = "404", description = "Products not found"),
-            @ApiResponse(responseCode = "500", description = "Unexpected error") })
-    default Flux<ResponseEntity<ProductCartResponse>> getProductsByCreteria(
-            @RequestParam(required = false) Map<String, Set<String>> filterCreteria) {
-        ProductCartResponse mockCartResponse = new ProductCartResponse("-1", "-1", "-1", BigInteger.valueOf(-1L), false);
-        return Flux.just(ResponseEntity.ok().body(mockCartResponse));
-    }
+  /**
+   * Exploring the products by filter criteria. Provide all products with given filters. If any tags
+   * provided then return all products.
+   *
+   * @param filterCreteria (non required params)
+   * @return OK (status code 200)
+   */
+  @GetMapping(value = "/products", produces = "application/json")
+  @Operation(
+      summary = "Get products by filter creteria",
+      description = "Get all products by specified filters")
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "OK",
+            content = {
+              @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(implementation = ProductCartResponse.class))
+            }),
+        @ApiResponse(
+            responseCode = "400",
+            description = "Bad request. Requested parameters doesn't exist"),
+        @ApiResponse(
+            responseCode = "401",
+            description = "Authorization information is missing or invalid"),
+        @ApiResponse(responseCode = "404", description = "Products not found"),
+        @ApiResponse(responseCode = "500", description = "Unexpected error")
+      })
+  default Flux<ResponseEntity<ProductCartResponse>> getProductsByCreteria(
+      @RequestParam(required = false) Map<String, Set<String>> filterCreteria) {
+    ProductCartResponse mockCartResponse =
+        new ProductCartResponse("-1", "-1", "-1", BigInteger.valueOf(-1L), false);
+    return Flux.just(ResponseEntity.ok().body(mockCartResponse));
+  }
 
-    /**
-     * Exploring exact product with all details GET /products/{productId}/details
-     * 
-     * @param productId (required)
-     * @return OK (status code 200)
-     */
-    @GetMapping(value = "/products/{productId}/details", produces = "application/json")
-    @Operation(summary = "Get product details", description = "Get details of a specific product by ID")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "OK", content = {
-                    @Content(mediaType = "application/json", schema = @Schema(implementation = ProductDetailsResponse.class)) }),
-            @ApiResponse(responseCode = "400", description = "Bad request. Requested parameter doesn't exist"),
-            @ApiResponse(responseCode = "401", description = "Authorization information is missing or invalid"),
-            @ApiResponse(responseCode = "404", description = "Product not found"),
-            @ApiResponse(responseCode = "500", description = "Unexpected error") })
-    default Flux<ResponseEntity<ProductDetailsResponse>> getProduct(
-            @Parameter(description = "ID of the product", required = true) @PathVariable String productId) {
-        ProductDetailsResponse mockResponse = new ProductDetailsResponse("-1", "-1", "-1", "-1", BigInteger.valueOf(-1L), "-1", false);
-        return Flux.just(ResponseEntity.ok().body(mockResponse));
-    }
+  /**
+   * Exploring exact product with all details GET /products/{productId}/details
+   *
+   * @param productId (required)
+   * @return OK (status code 200)
+   */
+  @GetMapping(value = "/products/{productId}/details", produces = "application/json")
+  @Operation(
+      summary = "Get product details",
+      description = "Get details of a specific product by ID")
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "OK",
+            content = {
+              @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(implementation = ProductDetailsResponse.class))
+            }),
+        @ApiResponse(
+            responseCode = "400",
+            description = "Bad request. Requested parameter doesn't exist"),
+        @ApiResponse(
+            responseCode = "401",
+            description = "Authorization information is missing or invalid"),
+        @ApiResponse(responseCode = "404", description = "Product not found"),
+        @ApiResponse(responseCode = "500", description = "Unexpected error")
+      })
+  default Flux<ResponseEntity<ProductDetailsResponse>> getProduct(
+      @Parameter(description = "ID of the product", required = true) @PathVariable
+          String productId) {
+    ProductDetailsResponse mockResponse =
+        new ProductDetailsResponse("-1", "-1", "-1", "-1", BigInteger.valueOf(-1L), "-1", false);
+    return Flux.just(ResponseEntity.ok().body(mockResponse));
+  }
 }
--- 3-platform/warehouse/spec/src/main/java/com/_6core/platform/warehousespec/rest/v1/dto/product/ProductCartResponse.java
+++ 3-platform/warehouse/spec/src/main/java/com/_6core/platform/warehousespec/rest/v1/dto/product/ProductCartResponse.java
@@ -2,6 +2,5 @@ package com._6core.platform.warehousespec.rest.v1.dto.product;
 
 import java.math.BigInteger;
 
-public record ProductCartResponse(String productId, String name, String image, BigInteger price, Boolean isAvailable) {
-
-}
+public record ProductCartResponse(
+    String productId, String name, String image, BigInteger price, Boolean isAvailable) {}
