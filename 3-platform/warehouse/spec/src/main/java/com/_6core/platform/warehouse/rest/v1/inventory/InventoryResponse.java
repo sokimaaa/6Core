f8ee5724ed19896225aa91fe91636d5f4007da93
Kateryna Mykhailovska <112807544+kateryna-mykh@users.noreply.github.com>
2024-03-25 12:39:41 +0200

    [6CORE-067] fix checkstyle to delivery, order, warehouse services (#68)
    * [6CORE-067] fixed checkstyle

--- 3-platform/warehouse/spec/src/main/java/com/_6core/platform/warehouse/rest/v1/inventory/InventoryResponse.java
+++
@@ -1,8 +0,0 @@
-package com._6core.platform.warehouse.rest.v1.inventory;
-
-public record InventoryResponse(
-        String inventoryId,
-        String productId,
-        Integer quantity
-) {
-}
---
+++ 3-platform/warehouse/spec/src/main/java/com/_6core/platform/warehousespec/rest/v1/inventory/InventoryResponse.java
@@ -0,0 +1,3 @@
+package com._6core.platform.warehousespec.rest.v1.inventory;
+
+public record InventoryResponse(String inventoryId, String productId, Integer quantity) {}
