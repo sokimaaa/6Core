b018091740d846cf37659b6c3c4abb2347c62a00
sokimaaa <romandenysov9@gmail.com>
2024-03-25 11:21:51 +0200

    [6CORE-000] fix violations for java

--- 3-platform/warehouse/spec/src/main/java/com/_6core/platform/warehousespec/rest/v1/WarehouseCreationApi.java
+++ 3-platform/warehouse/spec/src/main/java/com/_6core/platform/warehousespec/rest/v1/WarehouseCreationApi.java
@@ -13,35 +13,33 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import reactor.core.publisher.Flux;
 
-
-@Tag(name = "Warehouse creation specification",
-        description = "REST specification for warehouse creation")
+@Tag(
+    name = "Warehouse creation specification",
+    description = "REST specification for warehouse creation")
 public interface WarehouseCreationApi {
-    /**
-     * Create warehouse record by POST /warehouses
-     *
-     * @return CREATED (status code 201)
-     */
-    @Operation(summary = "Create warehouse record", description = "Create warehouse record")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "CREATED", content = {
-                    @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = WarehouseResponse.class))}),
-            @ApiResponse(responseCode = "400", description = "Field validation error"),
-            @ApiResponse(responseCode = "401", description = "Authentication error"),
-            @ApiResponse(responseCode = "500", description = "Internal server error")
-    })
-    @PostMapping("/warehouses")
-    default Flux<ResponseEntity<WarehouseResponse>> createWarehouse(
-            WarehouseRequest request
-    ) {
-        WarehouseResponse mockProduct = new WarehouseResponse(
-                "1002L",
-                "Warehouse address"
-        );
-        return Flux.just(ResponseEntity
-                .status(HttpStatusCode.valueOf(201))
-                .body(mockProduct));
-    }
+  /**
+   * Create warehouse record by POST /warehouses
+   *
+   * @return CREATED (status code 201)
+   */
+  @Operation(summary = "Create warehouse record", description = "Create warehouse record")
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "201",
+            description = "CREATED",
+            content = {
+              @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(implementation = WarehouseResponse.class))
+            }),
+        @ApiResponse(responseCode = "400", description = "Field validation error"),
+        @ApiResponse(responseCode = "401", description = "Authentication error"),
+        @ApiResponse(responseCode = "500", description = "Internal server error")
+      })
+  @PostMapping("/warehouses")
+  default Flux<ResponseEntity<WarehouseResponse>> createWarehouse(WarehouseRequest request) {
+    WarehouseResponse mockProduct = new WarehouseResponse("1002L", "Warehouse address");
+    return Flux.just(ResponseEntity.status(HttpStatusCode.valueOf(201)).body(mockProduct));
+  }
 }
-
